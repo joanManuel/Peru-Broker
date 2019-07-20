d40ed28c5d89a000e9e4862c496144c8a98935d3
joanManuel <joan.torres.jt@gmail.com>
2019-07-20 01:12:09 -0500

    Agregando tabs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Animación del navbar(menu)
 (function () {
 	$('nav.lista-menu').on('click', function() {
         $('.bar').toggleClass('animate'),
@@ -11,4 +12,28 @@ $(document).ready(function() {
 	$('nav.lista-menu').on('click', function() {
 		$('nav.lista-menu ul').toggle();
 	})
+});
+
+//SECCION NOSOTROS - TABS
+var tabs = document.querySelector('.tabs-buttons .swiper-wrapper');
+
+var tabButtons = new Swiper('.tabs-buttons', {
+  slidesPerView: 'auto',
+  freeMode: true,
+  scrollbar: '.swiper-scrollbar',
+  mousewheelControl: true,
+  onTap: function(swiper, event) {
+    if ( event.target.classList.contains('swiper-slide') && !event.target.classList.contains('active-tab') ) {
+      event.target.parentElement.querySelector('.active-tab').classList.remove('active-tab');
+      event.target.classList.add('active-tab');
+      tabContent.slideTo(swiper.clickedIndex);
+    }
+  }
+});
+
+var tabContent = new Swiper('.tabs-content', {
+  onSlideChangeStart: function(swiper, event) {
+    tabs.children[swiper.previousIndex].classList.remove('active');
+    tabs.children[swiper.activeIndex].classList.add('active');
+  }
 });
