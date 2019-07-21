aef062e9b6c6ffb4ed5eeeab9cbdad46df749133
joanManuel <joan.torres.jt@gmail.com>
2019-07-21 12:53:20 -0500

    Implementando slide en la seccion staff

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
   });
 
 //SECCION NOSOTROS - TABS
-var tabs = document.querySelector('.tabs-buttons .swiper-wrapper');
+var tabs = document.querySelector('.tabs-buttons');
 
 var tabButtons = new Swiper('.tabs-buttons', {
   slidesPerView: 'auto',
@@ -42,6 +42,7 @@ var tabButtons = new Swiper('.tabs-buttons', {
   mousewheelControl: true,
   onTap: function(swiper, event) {
     if ( event.target.classList.contains('swiper-slide') && !event.target.classList.contains('active-tab') ) {
+      
       event.target.parentElement.querySelector('.active-tab').classList.remove('active-tab');
       event.target.classList.add('active-tab');
       tabContent.slideTo(swiper.clickedIndex);
@@ -52,11 +53,50 @@ var tabButtons = new Swiper('.tabs-buttons', {
 var tabContent = new Swiper('.tabs-content', {
 
   // setWrapperSize:true,
+  // dragClass: '.swiper-slide-lock',
   autoHeight:true,
-
+  draggable:false,
+  // snapOnRelease:false,
   onSlideChangeStart: function(swiper, event) {
-    tabs.children[swiper.previousIndex].classList.remove('active');
-    tabs.children[swiper.activeIndex].classList.add('active');
-    
+    // tabs.children[swiper.previousIndex].classList.remove('active');
+    // tabs.children[swiper.activeIndex].classList.add('active');
+    // lockClass
   },
+});
+/*
+var asd = new Swiper('.item', {
+  
+    // dragClass: '.swiper-slide-lock'
+  
+});
+*/
+/**SECCION STAFF */
+/*
+var staff = document.querySelector('.content-item');
+var tabButtons = new Swiper('.content-item', {
+  slidesPerView: 1,
+  freeMode: true,
+  
+  mousewheelControl: true
+});
+*/
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var swiper = new Swiper('.content-item', {
+     slidesPerView: 'auto',
+          spaceBetween: 30,
+          centeredSlides: true,
+      breakpoints:{
+       
+       320: {
+          // slidesPerView: 'auto',
+          // spaceBetween: 30,
+          // centeredSlides: true,
+        }
+      }
+    // freeMode: true,
+    
+    // mousewheelControl: true
+  })
 });
