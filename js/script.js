9195fb5bfe8ed80226b73f1138a8614a309f33db
joanManuel <joan.torres.jt@gmail.com>
2019-07-22 16:20:23 -0500

    Arreglando el mapa

--- js/script.js
+++ js/script.js
@@ -111,8 +111,8 @@ $(document).ready(function () {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    L.marker([-12.126303, -77.016515]).addTo(map)
+        .bindPopup('PERU BROKER S.A.')
         .openPopup();
   
 });
